c6ace9652bc694f926a1d2384457407e77df640b
vardhman0000 <vardhman0000@gmail.com>
2024-08-07 17:42:55 +0530

    CRUD using MongoDB

--- expressMongoDB/index.js
+++ expressMongoDB/index.js
@@ -19,53 +19,79 @@ app.use(express.json());
 mongoose.connect(url, {
     useNewUrlParser:true, // builds string connection as url is given in string format
     useUnifiedTopology:true // maps server in a proper topology
-}).then(() => { 
-    StudentModel.insertMany([
-        {
-            name:"Vardhman Jain",
-            rollno:2513,
-            age:19
-        }
-    ])
-}).then(() => { 
-    console.log("Data Inserted!!");
-}).catch(err => console.error("Data not Inserted!!", err))
+})
+// .then(() => { 
+//     StudentModel.insertMany([
+//         {
+//             name:"Vardhman Jain",
+//             rollno:2513,
+//             age:19
+//         }
+//     ])
+// }).then(() => { 
+//     console.log("Data Inserted!!");
+// }).catch(err => console.error("Data not Inserted!!", err))
 
 
 
-app.get("/", (req,res) => { 
-    res.send("This is Home Page!!")
-    // let data = StudentModel.find()
-});
 
-// app.post("/saveStudent", (req,res) => { 
-//     let student = new StudentModel({
-//         name:"Stuxnet",
-//         rollno:85,
-//         age:19
-//     });
-//     student.save()
-//     res.send("Stuxnet Saved!!")
-// });
+
 
 
 /*
-    1, Data comes from req.body
-    2. Data initiated to model class
-    3. Data saved in DB
+1, Data comes from req.body
+2. Data initiated to model class
+3. Data saved in DB
 */
 
+// **** CREATE **** //
 app.post("/saveStudent", async (req,res) => { 
-    let student = new StudentModel(req.body);
+    let student = new StudentModel(req.body); // 1 , 2
     try{
-        const result = student.save();
+        const result = student.save(); // 3
         res.status(200).send(result);
     }
     catch(err){
         res.status(400).send(err);
     }
- });
+});
 
+// **** READ **** //
+app.get("/readData/:name", async (req,res) => { 
+    try{
+        let name = req.params.name;
+        let data = await StudentModel.find( {name : name} );
+        res.send(data);
+    }
+    catch(err){
+        res.send(err)
+    }
+});
+
+// **** UPDATE **** //
+app.put("/update/:rollno", async (req,res) => { 
+    try{
+        let rollno = req.params.rollno;
+        let name = req.body.name;
+        await StudentModel.updateOne( {rollno:rollno} , {$set : {name:name}} );
+        res.send("Data Updated!!")
+    }
+    catch(err){
+        res.send(err);
+    }
+})
+
+// **** DELETE **** //
+app.delete("/delete/:rollno", async (req,res) => { 
+    try{
+        let rollno = req.params.rollno;
+        await StudentModel.deleteOne({rollno:rollno});
+        res.send("Data Deleted!!")
+    }
+    catch(err){
+        res.send(err);
+    }
+});
 
 
 app.listen(port, () => { 
