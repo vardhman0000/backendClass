579fd29139010d1285d49a43e0a24924575548db
vardhman0000 <vardhman0000@gmail.com>
2024-08-21 17:45:23 +0530

    Basic MVC/Added gitignore file

--- againMVC/config/db.js
+++ againMVC/config/db.js
@@ -1,13 +1,6 @@
 let mongoose = require('mongoose');
-
-// const connection = mongoose.connect("mongodb+srv://amit:amitamit@cluster0.fxyt6.mongodb.net/infodb?retryWrites=true&w=majority&appName=Cluster0")
-//mongodb+srv://amit:amitamit@cluster0.fxyt6.mongodb.net/infodb?retryWrites=true&w=majority&appName=Cluster0
-
 require('dotenv').config()
 
 const connection = mongoose.connect(process.env.MONGODB_URL)
-
-// const connection = mongoose.connect("mongodb://127.0.0.1:27017/infodb")
-
 
 module.exports = {connection};
