0b52f8286a2c6593d0d396d23893e49ee9967924
vardhman0000 <vardhman0000@gmail.com>
2024-08-21 18:17:55 +0530

    Basic MVC/Added abc.js -> An OOPs Concept

---
+++ againMVC/abc.js
@@ -0,0 +1,9 @@
+class Student {
+    constructor(name, rollno, age){
+        this.name = name;
+        this.rollno = rollno;
+        this.age = age;
+    }
+}
+
+const student = new Student("Mydoom", 2513, 19);
--- againMVC/index.js
+++ againMVC/index.js
@@ -10,10 +10,23 @@ let app = express();
 app.use(express.json());
 app.use("/students", StudentRouter);
 
+//Global Error Handling
+app.use((err,req,res,next) => { 
+    res.status(err.statusCode).json({
+        message : err.message,
+    });
+});
 
 app.get("/", (req,res) => { 
     res.end("This is Home Page!!")
 })
+
+app.get("*", (req,res,next) => { 
+    // res.send({"message" : "Invalid URL", "status" : "ERROR"});
+    let err = new Error("Invalid URL");
+    err.statusCode = 404;
+    next(err);
+});
 
 
 app.listen(process.env.PORT, async () => { 
